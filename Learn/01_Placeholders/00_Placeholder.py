7ebef15184b21248d06d1302cec4ec49e04df847
Vinit Pardhi <91457234+VinitYP@users.noreply.github.com>
2023-02-12 16:46:19 +0530

    Update 00_Placeholder.py

--- Learn/01_Placeholders/00_Placeholder.py
+++ Learn/01_Placeholders/00_Placeholder.py
@@ -1,5 +1,5 @@
 # Placeholder is called as Taking the place of something
 sent = "%s is prime minister of india"
-print(sent% "Najendra Modi")
+print(sent% "narendra Modi")
 sent1 = "%s is %d years old"
-print(sent1%("Vinit",21))
+print(sent1%("Raju",21))
