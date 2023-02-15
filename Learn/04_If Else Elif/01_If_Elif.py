91b693e545ce4d30c6c8daf90ef3b7ad7d310dbd
Vinit Pardhi <91457234+VinitYP@users.noreply.github.com>
2023-02-15 15:43:45 +0530

    Add files via upload

---
+++ Learn/04_If Else Elif/01_If_Elif.py
@@ -0,0 +1,19 @@
+# if there are multiple condition, so we use if elif statement:
+
+age = int(input("Enter your age "))
+if (age >= 13 and age<=18):
+    print("Your are Teenager")
+elif (age <= 13):
+    print("You are Younger")
+else:
+    print("You are Adult")
+
+    
+"""
+        Equals: a == b
+        Not Equals: a != b
+        Less than: a < b
+        Less than or equal to: a <= b
+        Greater than: a > b
+        Greater than or equal to: a >= b
+"""
